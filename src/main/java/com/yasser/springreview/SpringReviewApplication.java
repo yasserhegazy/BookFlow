838bd53bca2dadd120af9034233aa0d01279e1e0
yasserhegazy <yasseranahegazy@gmail.com>
2026-06-05 22:53:25 +0300

    first commit

---
+++ src/main/java/com/yasser/springreview/SpringReviewApplication.java
@@ -0,0 +1,13 @@
+package com.yasser.springreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReviewApplication.class, args);
+	}
+
+}
