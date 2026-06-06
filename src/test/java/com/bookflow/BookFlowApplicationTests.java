f1fd32875a95c746592165d6e69f8c53faf99616
yasserhegazy <yasseranahegazy@gmail.com>
2026-06-06 10:47:00 +0300

    Add initial test class for BookFlow application

--- src/test/java/com/bookflow/BookFlowApplicationTests.java
+++ src/test/java/com/bookflow/BookFlowApplicationTests.java
@@ -1,10 +1,12 @@
-package com.yasser.springreview;
+package com.bookflow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
-class SpringReviewApplicationTests {
+class BookFlowApplicationTests {
 
 	@Test
 	void contextLoads() {
